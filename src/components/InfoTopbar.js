1c266856cc163c8eb5dd4c27622e5e07c0bb64ac
Maria Hannesdottir <mariaberghannesdottir@gmail.com>
2023-10-25 14:21:26 +0200

    test

--- src/components/InfoTopbar.js
+++ src/components/InfoTopbar.js
@@ -4,7 +4,7 @@ export default function InfoTopbar() {
   return (
     <div class="flex justify-center">
       <p class="text-white font-mono tracking-tighter my-2">
-        FRI FRAKT ÖVER 999KR
+        FRI FRAKT ÖVER 999KR.
       </p>
     </div>
   );
